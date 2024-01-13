9de14bccdafa3e152de353e9c0517f2bc8076b13
BigTogu <silvia.kenaan@gmail.com>
2024-01-13 22:51:02 +0100

    Add email confirmation page

---
+++ pages/email-confirmation/index.js
@@ -0,0 +1,7 @@
+import IconoJira from "../../public/assets/icono_Jira_Clone.svg";
+import Image from "next/image";
+import React, { useState, FormEvent } from "react";
+
+export default function EmailConfirmation() {
+  return <div>Hola</div>;
+}

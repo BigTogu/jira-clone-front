8147a750165eee17c68ea9b0363756a7f7824d11
BigTogu <silvia.kenaan@gmail.com>
2024-01-13 12:12:44 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF0000",
+        secondary: "#00FF00",
+      },
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

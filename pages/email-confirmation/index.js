2dd137ae0bbcabf0b4982f3b3961f6ad1a2cfdcc
BigTogu <silvia.kenaan@gmail.com>
2024-01-15 21:58:13 +0100

    Refactor email confirmation page

--- pages/email-confirmation/index.js
+++ pages/email-confirmation/index.js
@@ -1,7 +1,29 @@
-import IconoJira from "../../public/assets/icono_Jira_Clone.svg";
-import Image from "next/image";
-import React, { useState, FormEvent } from "react";
+import React, { useEffect, FormEvent } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function EmailConfirmation() {
+  const router = useRouter();
+  const { token } = router.query;
+  useEffect(() => {
+    if (!token) {
+      console.error("missing the token param on the url");
+      router.push("/register");
+      return;
+    }
+    fetch(`http://localhost:8000/email-confirmation/${token}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json().then((data) => {
+          router.push("/home");
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [router, token]);
+
   return <div>Hola</div>;
 }

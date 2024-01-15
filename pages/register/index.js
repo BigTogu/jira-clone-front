16ed367cb792002d51fe14b5bdd412288054256a
BigTogu <silvia.kenaan@gmail.com>
2024-01-15 21:58:01 +0100

    Add useRef hook and update layout in Register component

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { validateFields } from "../../utils/validate/index.js";
 import registerCall from "../../services/registerCall.js";
 import SuccessSubmit from "../../UI/succesSubmit/index.js";
@@ -31,22 +31,22 @@ const registerFields = [
   {
     type: "password",
   },
-  [
-    {
-      label: "Country",
-      name: "country",
-      type: "text",
-      placeholder: "Spain",
-      className: "",
-    },
-    {
-      label: "Telephone",
-      name: "telephone",
-      type: "text",
-      placeholder: "+34 601200422",
-      className: "",
-    },
-  ],
+  // [
+  //   {
+  //     label: "Country",
+  //     name: "country",
+  //     type: "text",
+  //     placeholder: "Spain",
+  //     className: "",
+  //   },
+  //   {
+  //     label: "Telephone",
+  //     name: "telephone",
+  //     type: "text",
+  //     placeholder: "+34 601200422",
+  //     className: "",
+  //   },
+  // ],
 ];
 
 export default function Register() {
@@ -70,21 +70,29 @@ export default function Register() {
     registerCall(data, setIsRegistrationSuccessful);
     event.currentTarget.reset();
   }
+  const boundingRef = useRef(null);
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen custom-gradient">
-      {!isRegistrationSuccessful ? (
-        <FormComponent
-          fields={registerFields}
-          handleSubmit={handleSubmit}
-          buttonText="Register"
-          buttonLabel="Log In"
-          linkInfo="/login"
-          descriptionLink="Already a Member?"
-        />
-      ) : (
-        <SuccessSubmit />
-      )}
+    <div className="w-screen h-screen flex flex-col md:flex-row items-center custom-gradient md:gap-11 lg:gap-24">
+      <div className="w-1/2 flex  justify-end flex-col md:items-end md:leading-[4rem] lg:leading-[6rem]  md:text-6xl font-bold text-end max-md:hidden">
+        <h2 className="">Be</h2>
+        <h2 className="">More</h2>
+        <h2 className="">Productive</h2>
+      </div>
+      <div className="flex flex-col justify-center items-start h-screen md:w-1/2 ">
+        {!isRegistrationSuccessful ? (
+          <FormComponent
+            fields={registerFields}
+            handleSubmit={handleSubmit}
+            buttonText="Register"
+            buttonLabel="Log In"
+            linkInfo="/login"
+            descriptionLink="Already a Member?"
+          />
+        ) : (
+          <SuccessSubmit />
+        )}
+      </div>
     </div>
   );
 }

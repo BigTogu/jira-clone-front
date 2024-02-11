9b6d88977a9d35b33cdd0242426efbf9cad49503
BigTogu <silvia.kenaan@gmail.com>
2024-02-11 10:25:54 +0100

    Refactor BoardForm component to use onSubmit prop

--- components/modal/CreateBoardForm/index.js
+++ components/modal/CreateBoardForm/index.js
@@ -1,21 +1,24 @@
-function BoardForm({ handleSubmit }) {
+function BoardForm({ onSubmit }) {
 	return (
-		<form onSubmit={handleSubmit} className="mb-2.5 flex items-center gap-3">
-			<label htmlFor="boardName" className="text-right  text-base">
-				Name
-			</label>
-			<input
-				id="boardName"
-				className="flex h-5 w-full flex-1 items-center rounded-md px-2.5 text-base shadow-md"
-				name="name"
-				placeholder="Frontend Project"
-				required
-				aria-required="true"
-			/>
+		<form onSubmit={onSubmit} className="mb-2.5 flex flex-col gap-3">
+			<div className="flex items-center gap-3">
+				<label htmlFor="boardName" className="text-right text-base">
+					Name:
+				</label>
+				<input
+					type="text"
+					id="boardName"
+					name="name"
+					placeholder="Frontend Project"
+					required
+					aria-required="true"
+					className="h-10 flex-1 rounded-md px-2.5 text-base shadow-md"
+				/>
+			</div>
 			<div className="mt-6 flex justify-end">
 				<button
 					type="submit"
-					className="rounded-xl  bg-green-400 px-4 py-2 text-white hover:bg-green-300"
+					className="rounded-xl bg-green-400 px-4 py-2 text-white hover:bg-green-300"
 					aria-label="Save the new board"
 				>
 					Save changes

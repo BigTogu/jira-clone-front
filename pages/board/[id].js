64d426885e404e52faf7c3817506d0fd6cf9697c
BigTogu <silvia.kenaan@gmail.com>
2024-02-11 20:08:19 +0100

    Add drag and drop functionality for reordering todos

--- pages/board/[id].js
+++ pages/board/[id].js
@@ -6,6 +6,7 @@ import { atom, useAtom } from 'jotai';
 import Modal from '../../components/Modal';
 import InvitationForm from '../../components/Modal/InvitationForm';
 import AssignedForm from '../../components/AssignedForm';
+import { Reorder } from 'framer-motion';
 
 export const todosAtom = atom([]);
 
@@ -54,14 +55,28 @@ export function BoardItem({ content }) {
 
 export function BoardColumn({ title, todos, status }) {
 	const [showCreateForm, setShowCreateForm] = useState(false);
+	const [, setItems] = useAtom(todosAtom);
+
+	function handleReorder(newOrder) {
+		setItems(newOrder);
+	}
+
 	return (
 		<div className="flex w-1/3 flex-col rounded bg-gray-100 p-4">
 			<h2 className="text-lg font-semibold">{title}</h2>
-			<div className="mt-4">
+			<Reorder.Group
+				axis="y"
+				values={todos}
+				onReorder={handleReorder}
+				className="mt-4"
+			>
 				{todos.map(todo => (
-					<BoardItem key={todo.id} content={todo.title} />
+					<Reorder.Item key={todo.id} value={todo}>
+						<BoardItem content={todo.title} />
+					</Reorder.Item>
 				))}
-			</div>{' '}
+			</Reorder.Group>
+
 			{showCreateForm ? (
 				<CreateTodoForm
 					onCancel={() => setShowCreateForm(false)}

ad911975c4cd4a884e2eb0ca36964fdbca86e26f
BigTogu <silvia.kenaan@gmail.com>
2024-02-12 22:23:55 +0100

    Refactor CreateBoard component

--- components/CreateBoard/index.js
+++ components/CreateBoard/index.js
@@ -1,34 +1,13 @@
 import Modal from '../Modal';
 import CreateBoardModal from '../Modal/CreateBoardModal';
-import { useState } from 'react';
 
 function CreateBoard() {
-	const [isOpen, setIsOpen] = useState(false);
-	const [errorMessage, setErrorMessage] = useState('');
-
-	function handleSuccess() {
-		setIsOpen(false);
-		setErrorMessage('');
-	}
-
-	function handleError(message) {
-		setErrorMessage(message);
-	}
-
 	return (
 		<Modal
-			open={isOpen}
-			setOpen={setIsOpen}
-			errorMessage={errorMessage}
 			triggerTitle={'Crear Proyecto'}
+			classNameTrigger={'rounded bg-blue-500 px-4 py-2 hover:bg-blue-400'}
 		>
-			<div>
-				<h2 className="text-lg font-semibold">Crear un nuevo tablero</h2>
-				<p className="my-2.5 text-base leading-4">
-					Invita a más personas. Haz click en guardar al terminar.
-				</p>
-				<CreateBoardModal onSuccess={handleSuccess} onError={handleError} />
-			</div>
+			<CreateBoardModal />
 		</Modal>
 	);
 }

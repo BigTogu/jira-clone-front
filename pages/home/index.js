870093686434e9dbc27c518cc3bc9909daa85894
BigTogu <silvia.kenaan@gmail.com>
2024-02-12 22:24:42 +0100

    Refactor home page component

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,15 +1,10 @@
-import ListBoards from '../../components/ListBoards/index.js';
-import CreateBoard from '../../components/CreateBoard/index.js';
+import ListBoards from '../../components/ListBoards';
+import Header from '../../components/Header/Home';
 
 export default function Home() {
 	return (
 		<section className="mx-7 my-9 flex flex-col">
-			<header className="mb-9 flex flex-row items-center justify-between">
-				<h1 className="text-xl font-bold">Proyectos</h1>
-				<div className="rounded bg-blue-500 px-4 py-2 hover:bg-blue-400">
-					<CreateBoard />
-				</div>
-			</header>
+			<Header />
 			<ListBoards />
 		</section>
 	);
